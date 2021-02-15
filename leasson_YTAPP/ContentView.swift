e2c2fd19fd429ae932a7a5a335d7dd4c7370716d
MCOMM00013 <muhammad.rahmatul@mncgroup.com>
2021-02-15 13:51:20 +0700

    Initial Commit

---
+++ leasson_YTAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  leasson_YTAPP
+//
+//  Created by Muhammad Khair on 15/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ leasson_YTAPP/leasson_YTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  leasson_YTAPPApp.swift
+//  leasson_YTAPP
+//
+//  Created by Muhammad Khair on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct leasson_YTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
